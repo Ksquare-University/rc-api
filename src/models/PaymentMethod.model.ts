d25570d2d1a454498231cd11d3905ad929c3af37
Rodrigo Leon <rodrigo.leon@itksquare.edu.mx>
2022-12-29 17:27:22 -0600

    Create Payment Method model

---
+++ src/models/PaymentMethod.model.ts
@@ -0,0 +1,36 @@
+import {
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  DataTypes,
+  Sequelize,
+} from "sequelize";
+
+export class PaymentMethod extends Model<
+  InferAttributes<PaymentMethod>,
+  InferCreationAttributes<PaymentMethod>
+> {
+  declare id: number;
+  declare method: string;
+  declare billingDetailsId: number;
+}
+
+export const initPaymentMethodModel = async (sequelize: Sequelize) => {
+  PaymentMethod.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      method: DataTypes.STRING,
+      billingDetailsId: DataTypes.INTEGER,
+    },
+    {
+      sequelize, // Instance of sequelize that reflects the connection
+    }
+  );
+
+  await PaymentMethod.sync();
+};
