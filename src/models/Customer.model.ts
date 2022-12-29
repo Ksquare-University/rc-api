b24f88a0ba8f0c6932625ad4fd193e1baa043f82
Brandon Hernandez <brandon.hernandez@itksquare.edu.mx>
2022-12-29 16:48:29 -0600

    client and sale models

---
+++ src/models/Customer.model.ts
@@ -0,0 +1,43 @@
+import { Model, InferAttributes, InferCreationAttributes, CreationOptional, DataTypes, Sequelize } from "sequelize";
+
+
+export class Client extends Model<InferAttributes<Client>, InferCreationAttributes<Client>> {
+
+    declare id: number;
+    declare full_name: string;
+    declare phone_number: number;
+    declare user_id: number;
+    declare status: boolean; 
+
+}
+
+export const initClientModel = async (sequelize: Sequelize) => {
+    Client.init({
+    
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        full_name: {
+            type: DataTypes.STRING,
+        },
+        phone_number: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.BOOLEAN,
+        }
+    
+}, {
+    sequelize // Instance of sequelize that reflects the connection
+})
+
+    await Client.sync();
+
+}
---
+++ src/models/Sale.model.ts
@@ -0,0 +1,63 @@
+import { Model, InferAttributes, InferCreationAttributes, CreationOptional, DataTypes, Sequelize } from "sequelize";
+
+
+export class Sale extends Model<InferAttributes<Sale>, InferCreationAttributes<Sale>> {
+
+    declare id: number;
+    declare order_id: number;
+    declare total_price: number;
+    declare total_items: number;
+    declare date: Date;
+    declare payment_id: number; 
+    declare address: string;
+    declare city_id: number;
+    declare refrences: string;
+    declare is_deleted: CreationOptional<boolean>;
+
+}
+
+export const initSaleModel = async (sequelize: Sequelize) => {
+    Sale.init({
+    
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        order_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+        },
+        total_price: {
+            type: DataTypes.DECIMAL
+        },
+        total_items: {
+            type: DataTypes.INTEGER
+        },
+        date: {
+            type: DataTypes.DATE
+        },
+        payment_id: {
+            type: DataTypes.INTEGER
+        },
+        address: {
+            type: DataTypes.STRING
+        },
+        city_id: {
+            type: DataTypes.INTEGER
+        },
+        refrences: {
+            type: DataTypes.STRING
+        },
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        }
+    
+}, {
+    sequelize // Instance of sequelize that reflects the connection
+})
+
+    await Sale.sync();
+
+}
