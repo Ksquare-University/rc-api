862e1a276207dfc14f112bdff6c48d59bbd18aa7
IvannaNouel-Ksquare <Ivanna.Nouel@itksquare.edu.mx>
2022-12-29 19:33:42 -0400

    Manager Model

---
+++ src/models/Manager.Model.ts
@@ -0,0 +1,23 @@
+import { Model, InferAttributes, InferCreationAttributes, CreationOptional, DataTypes, Sequelize } from "sequelize";
+
+export class Manager extends Model<InferAttributes<Manager>, InferCreationAttributes<Manager>> {
+    declare id: CreationOptional<string>;
+    declare restaurant_id: string;
+}
+
+export const initManager = (sequelize: Sequelize) => {
+    Manager.init({
+    
+        id: {
+            type: DataTypes.STRING,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        restaurant_id: {
+            type: DataTypes.STRING,
+        },
+
+}, { 
+    sequelize
+})
+}
