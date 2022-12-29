8c11a0b0a4146f7354f22c3142d2f4f10ad16d98
Rodrigo Leon <rodrigo.leon@itksquare.edu.mx>
2022-12-29 17:43:45 -0600

    Create Billing Details model

---
+++ src/models/BillingDetails.model.ts
@@ -0,0 +1,44 @@
+import {
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  DataTypes,
+  Sequelize,
+} from "sequelize";
+
+export class BillingDetails extends Model<
+  InferAttributes<BillingDetails>,
+  InferCreationAttributes<BillingDetails>
+> {
+  declare id: number;
+  declare ownerName: string;
+  declare billingAddress: string;
+  declare cardNumber: number;
+  declare cvv: number;
+  declare expirationDate: Date;
+  declare clientId: number;
+}
+
+export const initBillingDetailsModel = async (sequelize: Sequelize) => {
+  BillingDetails.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      ownerName: DataTypes.STRING,
+      billingAddress: DataTypes.STRING,
+      cardNumber: DataTypes.INTEGER,
+      cvv: DataTypes.INTEGER,
+      expirationDate: DataTypes.DATE,
+      clientId: DataTypes.INTEGER,
+    },
+    {
+      sequelize, // Instance of sequelize that reflects the connection
+    }
+  );
+
+  await BillingDetails.sync();
+};
