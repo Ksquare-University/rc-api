4be110b9b4b637461ee643a7eb45e00e6ec5923f
MarianaLopez-Ksquare <Mariana.Lopez@itksquare.edu.mx>
2022-12-30 12:38:12 -0600

    Opening Times Model

---
+++ src/models/OpeningTimes.model.ts
@@ -0,0 +1,30 @@
+import { Model, InferAttributes, InferCreationAttributes, CreationOptional, DataTypes, Sequelize } from "sequelize";
+
+
+export class OpeningTimes extends Model<InferAttributes<OpeningTimes>, InferCreationAttributes<OpeningTimes>> {
+
+    declare id: number;
+    declare restaurant_id : number;
+}
+
+export const initOpeningTimesModel = async (sequelize: Sequelize) => {
+    OpeningTimes.init({
+
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        restaurant_id :{
+            type:DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        }
+
+}, {
+    sequelize // Instance of sequelize that reflects the connection
+})
+
+await OpeningTimes.sync();
+
+}
