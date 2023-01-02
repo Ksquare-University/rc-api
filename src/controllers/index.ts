c73cfa7e97b0a3b13dbd2fce13e7b1959cb58c02
YumilRuedaFlores-Ksquare <Yumil.Flores@itksquare.edu.mx>
2023-01-02 09:57:57 +0100

    Endpoints Restaurant, TODO: add restructions

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
 import CityController from "./city.controller";
+import restaurantcontrollers from "./Restaurant.controllers";
 
 export default {
-    CityController,
+    CityController,restaurantcontrollers
 }
