74b2859283265bea7b6290d608be1c999f092eb0
Rodrigo Leon <rodrigo.leon@itksquare.edu.mx>
2023-01-03 15:13:27 -0600

    Add model into models array

---
+++ src/models/BillingDetails.model.ts
@@ -0,0 +1,85 @@
+import {
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+  DataTypes,
+  Sequelize,
+} from "sequelize";
+
+export class BillingDetails extends Model<
+  InferAttributes<BillingDetails>,
+  InferCreationAttributes<BillingDetails>
+> {
+  declare id: CreationOptional<number>;
+
+  declare owner_name: string;
+
+  declare billing_address: string;
+
+  declare card_number: number;
+
+  declare cvv: number;
+
+  declare expiration_date: Date;
+
+  declare client_id: number;
+}
+
+export const initBillingDetailsModel = async (sequelize: Sequelize) => {
+  BillingDetails.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+
+        autoIncrement: true,
+
+        primaryKey: true,
+      },
+
+      owner_name: {
+        type: DataTypes.INTEGER,
+
+        allowNull: false,
+      },
+
+      billing_address: {
+        type: DataTypes.STRING,
+
+        allowNull: false,
+      },
+
+      card_number: {
+        type: DataTypes.INTEGER,
+
+        allowNull: false,
+      },
+
+      cvv: {
+        type: DataTypes.INTEGER,
+
+        allowNull: false,
+      },
+
+      expiration_date: {
+        type: DataTypes.DATE,
+
+        allowNull: false,
+      },
+
+      client_id: {
+        type: DataTypes.INTEGER,
+
+        allowNull: false,
+      },
+    },
+
+    {
+      tableName: "billing_details",
+
+      sequelize, // Instance of sequelize that reflects the connection
+    }
+  );
+
+  await BillingDetails.sync();
+};
