cbb3d4b5c175d7193dd7a92b6331e1d1b7827815
Genaro Arredondo <Genaro.Arredondo@itksquare.edu.mx>
2023-01-03 16:29:54 -0600

    Fixed description on ClientAddress Model

--- src/models/ClientAddress.model.ts
+++ src/models/ClientAddress.model.ts
@@ -3,13 +3,13 @@ import { Model, InferAttributes, InferCreationAttributes, CreationOptional, Data
 
 export class ClientAddress extends Model<InferAttributes<ClientAddress>, InferCreationAttributes<ClientAddress>> {
 
-    declare id: number;
+    declare id: CreationOptional<number>;
     declare client_id: number;
     declare address: string;
     declare reference: string;
     declare zip_code: number;
     declare city_id: number;
-    declare is_deleted: boolean;
+    declare is_deleted: CreationOptional<boolean>;
 
 }
 
@@ -21,14 +21,33 @@ export const initClientAddressModel = async (sequelize: Sequelize) => {
             autoIncrement: true,
             primaryKey: true
         },
-        client_id: DataTypes.INTEGER,
-        address: DataTypes.STRING,
-        reference: DataTypes.STRING,
-        zip_code: DataTypes.INTEGER,
-        city_id: DataTypes.INTEGER,
-        is_deleted: DataTypes.BOOLEAN
+        client_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        reference: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        zip_code: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        city_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
     
 }, {
+    tableName: 'client_address',
     sequelize // Instance of sequelize that reflects the connection
 })
 
