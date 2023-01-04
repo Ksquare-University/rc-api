09dcebbf1f5d167c886171b71d8b659ec24d57df
MarianaLopez-Ksquare <Mariana.Lopez@itksquare.edu.mx>
2023-01-04 10:39:56 -0600

    is_deleted added

--- src/models/OpeningTimes.model.ts
+++ src/models/OpeningTimes.model.ts
@@ -19,7 +19,11 @@ export const initOpeningTimesModel = async (sequelize: Sequelize) => {
         restaurant_id :{
             type:DataTypes.INTEGER,
             allowNull: false,
-        }
+        },
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+          },
 
 }, {
     tableName: 'opening_times',
