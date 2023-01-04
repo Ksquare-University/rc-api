1a41443f0c7911d816768fcd95cdc0a660979d12
“Dannaé <claudia.diaz@itksquare.edu.mx>
2023-01-03 20:23:28 -0600

    Connected users route to express app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application, Request, Response } from 'express';
+// Import routers
 import cityRoutes  from "./routes/city.routes"
+import { UserRouter } from './routes/user.routes'
 
 const app: Application = express();
 app.use(express.json());
 
 app.use('/city', cityRoutes);
+// Connect routers
+app.use('/users', UserRouter); 
 
 app.get('/', (req: Request, res: Response) => {
     res.send('This App is alive :D');
