642dfabddcdf26f44ad074ec4da3a41787a22943
BrandonHernandez-Ksquare <113386913+BrandonHernandez-Ksquare@users.noreply.github.com>
2023-01-05 12:54:44 -0600

    Task 19594 19605 (#8)
    * client and sale models
    
    * quick fix
    
    * refrences changed to creationOptional
    
    * final touches
    
    * ignored dist folder
    
    * added tableName
    
    * name not allowed as null
    
    * resolved changes to status, user_id and references
    
    * id set to created optional

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,11 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize
+import { initClientModel } from "./Customer.model";
+import { initSaleModel } from "./Sale.model";
 import { initUserModel } from "./user.model";
 import { initCity } from "./city.model";
 
 export let sequelize: Sequelize;
-const models = [initCity, initUserModel];
+const models = [initCity, initUserModel, initClientModel, initSaleModel];
 
 export const startSequelize = (db_name: string, db_password: string, db_hostname: string, db_username: string) => {
     sequelize = new Sequelize(db_name, db_username, db_password, {
