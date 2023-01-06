f47e87da9e212d3c02e50f10947e01afc7a90fe7
Yumil Rueda Flores <113385187+YumilRuedaFlores-Ksquare@users.noreply.github.com>
2023-01-06 01:56:23 +0100

    Role Model (#13)
    * Role Model
    
    * Update Role.model.ts
    
    * Add tableName
    
    * role name
    
    * allowNull false
    
    * Role Model
    
    * Update Role.model.ts
    
    * Add tableName
    
    * role name
    
    * allowNull false
    
    * ,
    
    * A

---
+++ src/models/Role.model.ts
@@ -0,0 +1,32 @@
+import { Model, InferAttributes, InferCreationAttributes, CreationOptional, DataTypes, Sequelize } from "sequelize";
+
+
+export class Role extends Model<InferAttributes<Role>, InferCreationAttributes<Role>> {
+
+    declare id: CreationOptional<number>;
+    declare role: string;
+
+}
+
+export const initRoleModel = async (sequelize: Sequelize) => {
+    Role.init({
+    
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    
+}, {
+    tableName: "role",
+    sequelize // Instance of sequelize that reflects the connection
+})
+
+    await Role.sync();
+
+}
+
