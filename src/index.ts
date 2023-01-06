6bf79ef94def4c0c21d14b0fcb8b9b22e9134e9f
“Dannaé <claudia.diaz@itksquare.edu.mx>
2023-01-06 09:27:35 -0600

    Refactored start sequelize function back to normal

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,18 @@ import app from "./app";
 admin.initializeApp(); // For firebase => loads credentials
 
 const PORT = <string>process.env.PORT;
-const DB = <string>process.env.DB_NAME;
-const PASSWD = <string>process.env.DB_PASS;
-const USER = <string>process.env.DB_USER;
-const HOST = <string>process.env.localhost;
-
+const DB_NAME = <string>process.env.DB_NAME;
+const DB_PASS = <string>process.env.DB_PASS;
+const DB_USER = <string>process.env.DB_USER;
+const DB_HOST = <string>process.env.localhost;
 
 app.listen(PORT, async () => {
     try {
-        const sequelize = startSequelize(DB, PASSWD, HOST, USER);
-                await sequelize.sync(); 
-                console.info('DB and Express server is up and running!!!!')
-            } catch (error) {
-                console.error(error);
-                process.abort();
-            }
+        const sequelize = startSequelize(DB_NAME, DB_PASS, DB_HOST, DB_USER);
+        await sequelize.sync(); 
+        console.info('DB and Express server is up and running!') 
+    } catch (error) {
+        console.error(error);
+        process.abort(); 
+    }
 })
