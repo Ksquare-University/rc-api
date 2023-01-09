be409d9c896788ab99ae1dc4aa29c2cec6c933d2
“Dannaé <claudia.diaz@itksquare.edu.mx>
2023-01-09 16:22:16 -0600

    Granted users' permissions

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,27 +8,32 @@ import {
   updateUser,
   enableUser,
 } from "../firebase";
-// Still need to create middlewares to use them in this file
-// import { isAuthenticated } from "../middlewares/isAuthenticated";
-// import { isAuthorized } from "../middlewares/isAuthorized";
+import { isAuthenticated } from "../middlewares/isAuthenticated";
+import { isAuthorized } from "../middlewares/isAuthorized";
 
 export const UserRouter = Router();
 
 // ==================================  L C R U D  ==================================
 
 // LIST - [GET] all
-UserRouter.get("/", async (req: Request, res: Response) => {
-  try {
-    const listedUsers = await getAllUsers();
+// Only superadmins and admins can have access to all users info
+UserRouter.get(
+  "/",
+  isAuthenticated,
+  isAuthorized({ roles: ["superadmin", "admin"], allowSameUser: true }),
+  async (req: Request, res: Response) => {
+    try {
+      const listedUsers = await getAllUsers();
 
-    res.status(200).send({ listedUsers });
-  } catch (error) {
-    res.status(400).send({ error: "Couldn't list users. Verify the route" });
+      res.status(200).send({ listedUsers });
+    } catch (error) {
+      res.status(400).send({ error: "Couldn't list users. Verify the route" });
+    }
   }
-});
+);
 
 // CREATE - [POST]
-// Allow a customer to sign up to our system
+// Allow anyone to signup as a customer to rappi, no auth needed
 UserRouter.post("/customer", async (req: Request, res: Response) => {
   const { displayName, email, password } = req.body;
 
@@ -55,157 +60,204 @@ UserRouter.post("/customer", async (req: Request, res: Response) => {
   }
 });
 
-// Only allow admins and superadmins to create owners (rest owners)
-UserRouter.post("/owner", async (req: Request, res: Response) => {
-  const { displayName, email, password } = req.body;
+// Only allow admins and superadmins to create restaurant owners
+UserRouter.post(
+  "/owner",
+  isAuthenticated,
+  isAuthorized({ roles: ["superadmin", "admin"], allowSameUser: true }),
+  async (req: Request, res: Response) => {
+    const { displayName, email, password } = req.body;
 
-  if (!displayName || !email || !password) {
-    return res.status(400).send({ error: "Missing or incorrect fields" });
-  }
+    if (!displayName || !email || !password) {
+      return res.status(400).send({ error: "Missing or incorrect fields" });
+    }
 
-  try {
-    const newOwnerId = await createUser(displayName, email, password, "owner");
+    try {
+      const newOwnerId = await createUser(
+        displayName,
+        email,
+        password,
+        "owner"
+      );
 
-    res.status(201).send({
-      success: "owner created successfully!",
-      id: newOwnerId,
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .send({
+      res.status(201).send({
+        success: "owner created successfully!",
+        id: newOwnerId,
+      });
+    } catch (error) {
+      res.status(500).send({
         error: "Something went wrong, couldn't create restaurant owner.",
       });
+    }
   }
-});
+);
 
 // Only allow super admin (?) to create new admins
-UserRouter.post("/admin", async (req: Request, res: Response) => {
-  const { displayName, email, password } = req.body;
+UserRouter.post(
+  "/admin",
+  isAuthenticated,
+  isAuthorized({ roles: ["superadmin"], allowSameUser: true }),
+  async (req: Request, res: Response) => {
+    const { displayName, email, password } = req.body;
 
-  if (!displayName || !email || !password) {
-    return res.status(400).send({ error: "Missing or incorrect fields" });
+    if (!displayName || !email || !password) {
+      return res.status(400).send({ error: "Missing or incorrect fields" });
+    }
+
+    try {
+      const newAdminId = await createUser(
+        displayName,
+        email,
+        password,
+        "admin"
+      );
+
+      res.status(201).send({
+        success: "Admin created successfully!",
+        id: newAdminId,
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .send({ error: "Something went wrong, couldn't create admin." });
+    }
   }
-
-  try {
-    const newAdminId = await createUser(displayName, email, password, "admin");
-
-    res.status(201).send({
-      success: "Admin created successfully!",
-      id: newAdminId,
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .send({ error: "Something went wrong, couldn't create admin." });
-  }
-});
+);
 
 // READ - [GET]
-UserRouter.get("/:userId", async (req: Request, res: Response) => {
-  const id: string = req.params["userId"];
+UserRouter.get(
+  "/:userId",
+  isAuthenticated,
+  isAuthorized({
+    roles: ["superadmin", "admin", "owner", "customer"],
+    allowSameUser: true,
+  }),
+  async (req: Request, res: Response) => {
+    const id: string = req.params["userId"];
 
-  if (+id <= 0) {
-    return res.status(400).send({
-      error: "Invalid id",
-    });
+    if (+id <= 0) {
+      return res.status(400).send({
+        error: "Invalid id",
+      });
+    }
+
+    try {
+      const fetchedUser = await readUser(id);
+
+      res.status(200).send({ fetchedUser });
+    } catch (error) {
+      res
+        .status(400)
+        .send({
+          error: "Couldn't read user. The requested route doesn't exist",
+        });
+    }
   }
-
-  try {
-    const fetchedUser = await readUser(id);
-
-    res.status(200).send({ fetchedUser });
-  } catch (error) {
-    res
-      .status(400)
-      .send({ error: "Couldn't read user. The requested route doesn't exist" });
-  }
-});
+);
 
 // UPDATE - [PUT]
-UserRouter.put("/:userId", async (req: Request, res: Response) => {
-  const id: string = req.params["userId"];
+UserRouter.put(
+  "/:userId",
+  isAuthenticated,
+  isAuthorized({
+    roles: ["superadmin", "admin", "owner", "customer"],
+    allowSameUser: true,
+  }),
+  async (req: Request, res: Response) => {
+    const id: string = req.params["userId"];
 
-  if (+id <= 0) {
-    return res.status(400).send({
-      error: "Invalid id",
-    });
+    if (+id <= 0) {
+      return res.status(400).send({
+        error: "Invalid id",
+      });
+    }
+    const { displayName, email, password } = req.body;
+
+    if (!displayName || !email || !password) {
+      return res.status(400).send({ error: "Missing or incorrect fields" });
+    }
+
+    try {
+      const updatedUser = await updateUser(id, displayName, email, password);
+
+      res.status(200).send({ updatedUser });
+    } catch (error) {
+      res
+        .status(400)
+        .send({ error: "Couldn't update user. Verify the requested user ID" });
+    }
   }
-  const { displayName, email, password } = req.body;
-
-  if (!displayName || !email || !password) {
-    return res.status(400).send({ error: "Missing or incorrect fields" });
-  }
-
-  try {
-    const updatedUser = await updateUser(id, displayName, email, password);
-
-    res.status(200).send({ updatedUser });
-  } catch (error) {
-    res
-      .status(400)
-      .send({ error: "Couldn't update user. Verify the requested user ID" });
-  }
-});
+);
 
 // DELETE - [DELETE]
 // A user can call an endpoint to disable its account (this is a soft-delete operation)
-UserRouter.delete("/:userId", async (req: Request, res: Response) => {
-  const id: string = req.params["userId"];
-  const userInfo = await admin.auth().getUser(id);
+UserRouter.delete(
+  "/:userId",
+  isAuthenticated,
+  isAuthorized({
+    roles: ["superadmin", "admin", "owner", "customer"],
+    allowSameUser: true,
+  }),
+  async (req: Request, res: Response) => {
+    const id: string = req.params["userId"];
+    const userInfo = await admin.auth().getUser(id);
 
-  if (+id <= 0) {
-    return res.status(400).send({
-      error: "Invalid id",
-    });
+    if (+id <= 0) {
+      return res.status(400).send({
+        error: "Invalid id",
+      });
+    }
+
+    if (userInfo.disabled) {
+      return res.status(200).send({
+        message:
+          "Nothing to do here, this account is already disabled. Try a different user id.",
+      });
+    }
+
+    try {
+      const disabledUser = await disableUser(id);
+
+      res.status(200).send({ disabledUser });
+    } catch (error) {
+      res
+        .status(400)
+        .send({ error: "Couldn't disable user. Verify the requested user ID" });
+    }
   }
-
-  if (userInfo.disabled) {
-    return res.status(200).send({
-      message:
-        "Nothing to do here, this account is already disabled. Try a different user id.",
-    });
-  }
-
-  try {
-    const disabledUser = await disableUser(id);
-
-    res.status(200).send({ disabledUser });
-  } catch (error) {
-    res
-      .status(400)
-      .send({ error: "Couldn't disable user. Verify the requested user ID" });
-  }
-});
+);
 
 // UNDO DELETE - [PATCH]
-// Admins can modify the disabled property from the User model back to false.
-UserRouter.patch("/activate/:userId", async (req: Request, res: Response) => {
-  const id: string = req.params["userId"];
-  const userInfo = await admin.auth().getUser(id);
+// Admins and superadmins can modify the disabled property from the User model back to false.
+UserRouter.patch(
+  "/activate/:userId",
+  isAuthenticated,
+  isAuthorized({ roles: ["superadmin", "admin"], allowSameUser: true }),
+  async (req: Request, res: Response) => {
+    const id: string = req.params["userId"];
+    const userInfo = await admin.auth().getUser(id);
 
-  if (+id <= 0) {
-    return res.status(400).send({
-      error: "Invalid id",
-    });
-  }
+    if (+id <= 0) {
+      return res.status(400).send({
+        error: "Invalid id",
+      });
+    }
 
-  if (!userInfo.disabled) {
-    return res.status(200).send({
-      message:
-        "Nothing to do here, this account is already active. Try a different user id.",
-    });
-  }
+    if (!userInfo.disabled) {
+      return res.status(200).send({
+        message:
+          "Nothing to do here, this account is already active. Try a different user id.",
+      });
+    }
 
-  try {
-    const enabledUser = await enableUser(id);
+    try {
+      const enabledUser = await enableUser(id);
 
-    res.status(200).send({ enabledUser });
-  } catch (error) {
-    res
-      .status(400)
-      .send({
+      res.status(200).send({ enabledUser });
+    } catch (error) {
+      res.status(400).send({
         error: "Couldn't enable user. Verify the requested path or user ID",
       });
+    }
   }
-});
+);
