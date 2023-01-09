e723d1cfd5d9b71b7c9d2c84d21ddc9bcf5feab2
“Dannaé <claudia.diaz@itksquare.edu.mx>
2023-01-09 16:22:16 -0600

    Granted users' permissions

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,9 @@ import { UserRouter } from './routes/user.routes'
 const app: Application = express();
 app.use(express.json());
 
-app.use('/city', cityRoutes);
 // Connect routers
 app.use("/users", UserRouter);
+app.use('/city', cityRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("This App is alive :D");
