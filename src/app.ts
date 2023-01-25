3b5b45e0267a9c09972650904cab8d3d9940c40f
JonathanCastillo-Ksquare <Jonathan.Castillo@itksquare.edu.mx>
2023-01-25 16:33:44 -0600

    webSocket implementation

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { UserRouter } from "./routes/user.routes";
 import cityRoutes from "./routes/city.routes";
 import restaurantRouter from "./routes/restaurants.routes";
 import scheduleRouter from './routes/Schedules.routes'
+import orderRoutes from "./routes/order.routes";
+import orderItemsRoutes from "./routes/orderItems.routes";
 // Import cors in order to access MLHTTPRequest from Origin LocalHost:3000 (React App)
 var cors = require('cors')
 
@@ -16,6 +18,7 @@ app.use("/users", UserRouter);
 app.use("/city", cityRoutes);
 app.use("/restaurant", restaurantRouter);
 app.use('/schedule', scheduleRouter);
-
+app.use('/orders', orderRoutes);
+app.use('/orderitems', orderItemsRoutes);
 
 export default app;
---
+++ src/controllers/orderItems.controller.ts
@@ -0,0 +1,102 @@
+import { Request, Response } from "express";
+import { OrderItems } from "../models/OrderItems.model";
+import { Item } from "../models/Item.model";
+
+const orderItemsController = {
+    getOrderItemsByOrderId: async (req: Request, res: Response) => {
+        try {
+            const { orderId } = req.params;
+            const foundItems = await OrderItems.findAll({
+                where: {
+                    order_id: orderId
+                },
+                attributes: ['id', 'order_id', 'item_id', 'quantity', 'is_deleted']
+            })
+
+            if (!foundItems) {
+                return res.status(404).json({
+                    error: "Order not found!"
+                });
+
+            }
+
+            return res.status(200).send(foundItems);
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({ error: "Something went wrong!" })
+        }
+    },
+    addItemsToOrder: async (req: Request, res: Response) => {
+        try {
+            const { orderId } = req.params;
+            const { itemId, quantity } = req.body;
+
+            if (!itemId || quantity <= 0) {
+                return res.status(400).json({ error: "No item to add!" });
+            }
+
+            await OrderItems.create({
+                item_id: itemId,
+                order_id: Number(orderId),
+                quantity: quantity
+            });
+
+            return res.status(200).json({ success: `Item added to order ${orderId}` });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Something went wrong!" });
+        }
+    },
+    updateItemquantity: async (req: Request, res: Response) => {
+        try {
+            const { orderId } = req.params;
+            const { itemId, quantity } = req.body;
+
+            if (!itemId || !quantity) {
+                {
+                    return res.status(400).json({ error: "No item to update!" });
+                }
+            }
+
+            await OrderItems.update({
+                quantity: quantity
+            }, {
+                where: {
+                    item_id: itemId,
+                    order_id: orderId
+                }
+            });
+
+            return res.status(200).json({ success: "Item updated successfully!" });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Something went wrong!" });
+        }
+    },
+    deleteItem: async (req: Request, res: Response) => {
+        try {
+            const { orderId } = req.params;
+            const { itemId } = req.body;
+
+            if (!itemId) {
+                return res.status(400).json({ error: "No item to delete!" });
+            }
+
+            await OrderItems.destroy({
+                where: {
+                    order_id: orderId,
+                    item_id: itemId
+                }
+            });
+
+            return res.status(200).json({ success: "Item deleted successfully!" });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error: "Something went wrong!" });
+        }
+
+    }
+
+};
+
+export default orderItemsController;
